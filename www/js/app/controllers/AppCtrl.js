063e8aa81170d4e3cf9fccdd48a520ae62dbab3b
pa.otoya757@uniandes.edu.co <pa.otoya757@uniandes.edu.co>
2016-04-14 17:08:57 -0500

    das

--- www/js/app/controllers/AppCtrl.js
+++ www/js/app/controllers/AppCtrl.js
@@ -4,19 +4,6 @@
 angular.module('tickitup')
     .controller('AppCtrl',function($scope,$state,ClienteService){
 
-        //this will represent our logged user
-        var user = {
-            about: "Design Lead of Project Fi. Love adventures, green tea, and the color pink.",
-            name: "Brynn Evans",
-            picture: "https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg",
-            _id: 0,
-            followers: 345,
-            following: 58
-        };
-
-        //save our logged user on the localStorage
-        $scope.loggedUser = user;
-
         /**
          * Logs the current user out
          */
